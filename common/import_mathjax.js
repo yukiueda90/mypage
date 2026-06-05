b41162dfe037e13f92e57a4225c4e1ca37d8a915
Yuki Ueda <yukiueda.90@gmail.com>
2026-06-05 17:36:12 +0900

    comment out about polyfill

--- common/import_mathjax.js
+++ common/import_mathjax.js
@@ -16,11 +16,11 @@ mathjaxScript.id = 'MathJax-script';
 mathjaxScript.src = 'https://cdn.jsdelivr.net/npm/mathjax@3/es5/tex-mml-chtml.js';
 mathjaxScript.async = true;
 
-// Create a script element for polyfill.io
-var polyfillScript = document.createElement('script');
-polyfillScript.src = 'https://polyfill.io/v3/polyfill.min.js?features=es6';
+// // Create a script element for polyfill.io
+// var polyfillScript = document.createElement('script');
+// polyfillScript.src = 'https://polyfill.io/v3/polyfill.min.js?features=es6';
 
 // Append both script elements to the document's head
 document.head.appendChild(mathjaxConfigScript);
 document.head.appendChild(mathjaxScript);
-document.head.appendChild(polyfillScript);
+// document.head.appendChild(polyfillScript);
